2f74beb21c6cd83cfc3f5ad29a848eedc4ff4d08
noa-3743 <48903208+noa-3743@users.noreply.github.com>
2019-05-09 09:50:17 +0800

    copy

---
+++ copy.cpp
@@ -0,0 +1,53 @@
+// copy.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "iostream"
+using namespace std;
+class CTestCopy
+{
+	int x;
+	int y;
+public:
+	CTestCopy();
+	CTestCopy(int a, int b);
+	CTestCopy(CTestCopy & om);
+	void setXY(int a, int b);
+	void disPlay();
+};
+CTestCopy::CTestCopy()
+{
+
+}
+CTestCopy::CTestCopy(int a, int b)
+{
+	x = a;
+	y = b;
+}
+CTestCopy::CTestCopy(CTestCopy & om)
+{
+	x = om.x * 2;
+	y = om.y * 2;
+	cout << "****************" << endl;
+}
+void CTestCopy::setXY(int a, int b)
+{
+	x = a;
+	y = b;
+}
+void CTestCopy::disPlay()
+{
+	cout <<"x=" << x << " " << "y=" << y << endl;
+}
+int MyTest(CTestCopy temp)
+{
+	temp.disPlay();
+	return 0;
+}
+int main()
+{
+	CTestCopy ot(50, 100);
+	MyTest(ot);
+	return 0;
+}
+
